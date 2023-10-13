7f413f00a9b7d20812961e9a78dfe62ed5ed658a
brattmadshaw <92447517+brattmadshaw@users.noreply.github.com>
2023-10-13 13:51:56 +0100

    Update selection_part1.py IF to ELIF

--- selection_part1.py
+++ selection_part1.py
@@ -1,7 +1,7 @@
 # this will take users input and add into the variable 'age' 
 age = int(input("Enter your age:"))
 
-# variable 'age' will then be put into correct category based on value, using IF statement this will display both A and B over the age of 18
+# variable 'age' will then be put into correct category based on value, using ELIF statement this will now display the correct value in comparison to IF statement.
 if age >= 18:
     print("You are in category A")
 elif age >= 16:
