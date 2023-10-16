e7210168f4f8b88bcd0bc1b6316fa4c4c85ddfaa
Matt Bradshaw <mbradshaw0315@gmail.com>
2023-10-16 10:38:51 +0100

    added via git

---
+++ Chapter2-Lab2/lab1/completed-lab1_task3_get_user_input.py
@@ -0,0 +1,8 @@
+username=input('Please enter your name')
+age = input('Please enter your age')
+print(username,'is',age,'years old')
+
+## my code
+name = input('what is your name: ')
+age = input('how old are you: ')
+print('hello', name, 'you are', age)
---
+++ Chapter2-Lab2/lab2/completed-lab2_task2_selection_part2_calculator.py
@@ -0,0 +1,28 @@
+print('''Welcome to the lab2 calculator.py
+please select from the menu below
+1. Add +
+2. Subtract -
+3. Multiply *
+4. Divide /
+5. Square s 
+''')
+
+calculation = input('please select your calculation choice (1-5): ')
+
+print('you have selected', calculation)
+
+number_one = float(input('please enter the first number: '))
+number_two = float(input('please enter the second number: '))
+
+if calculation == '1':
+    print(number_one + number_two)
+elif calculation == '2':
+    print(number_one - number_two)
+elif calculation == '3':
+    print(number_one * number_two)
+elif calculation == '4':
+        print(number_one / number_two)
+elif calculation == '5':
+    print(number_one ** 2)
+else:
+    print("Invalid choice. Please select a calculation between 1 and 5.")
