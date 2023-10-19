de485224495c92add318fde61c8893639ab79eb1
brattmadshaw <92447517+brattmadshaw@users.noreply.github.com>
2023-10-19 14:49:35 +0100

    Delete Chapter2-Lab2/completed-lab1 directory

--- Chapter2-Lab2/completed-lab1/completed-lab1_task2_display_message.py
+++
@@ -1,3 +0,0 @@
-username='Bob'
-age=32
-print(username,'is',age,'years old')
--- Chapter2-Lab2/completed-lab1/completed-lab1_task3_get_user_input.py
+++
@@ -1,8 +0,0 @@
-username=input('Please enter your name')
-age = input('Please enter your age')
-print(username,'is',age,'years old')
-
-## my code
-name = input('what is your name: ')
-age = input('how old are you: ')
-print('hello', name, 'you are', age)
